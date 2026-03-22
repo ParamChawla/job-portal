5da51265ff329a155eb65e21d5ac812f6e342b1a
Param Chawla <paramchawla060606@gmail.com>
2026-03-22 23:52:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,40 +7,29 @@ import userRoute from './routes/user.route.js';
 import companyRoute from './routes/company.route.js';
 import jobRoute from './routes/job.route.js';
 import applicationRoute from './routes/application.route.js';
+
 dotenv.config({});
-
-
 
 const app = express();
 
-
-
-
-//midleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your frontend URL
-    credentials: true, // Allow cookies to be sent
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
 };
-
-
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 
-//routes api
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
-
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server is running on port ${PORT}`);
-
 });
